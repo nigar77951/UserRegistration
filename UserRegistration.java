1f0c08368d246d28bc47b599d1d81ae4f681c124
unknown <nigar77951@gmail.com>
2021-01-15 22:19:28 +0530

    validating user email

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,20 +1,22 @@
-import java.util.regex.*;
-import java.util.*;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter last Name:");
-		String lname=sc.nextLine();
-		if(lname == null)
+		System.out.println("Enter an email:");
+		String email=sc.nextLine();
+		if(email == null)
 			System.out.println("Invalid");
-		String RegExlname = "^[A-Z]{1}[a-z]{2,}$";
-	    Pattern pattern = Pattern.compile(RegExlname);
-		if(pattern.matcher(lname).matches()) {
+		String RegExemail = "^[A-Za-z0-9_]+[A-Za-z0-9_-]*([.]{1}[a-zA-Z-0-9]*)*[@]{1}[a-z]+[.][a-z]{2,6}([.][a-z0-9]{2})*$";
+	    Pattern pattern = Pattern.compile(RegExemail);
+		if(pattern.matcher(email).matches()) {
 			System.out.println("valid");
 		} else {
 			System.out.println("Invalid"); 
 		}
 	}
-}
+	}
