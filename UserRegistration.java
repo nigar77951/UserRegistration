b2ee16d6912b814ea4c4435a62f6917779c2ea73
unknown <nigar77951@gmail.com>
2021-01-15 21:28:45 +0530

    user enters valid last name

--- UserRegistration.java
+++ UserRegistration.java
@@ -5,13 +5,13 @@ public class UserRegistration {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter first Name:");
-		String fname=sc.nextLine();
-		if(fname == null)
+		System.out.println("Enter last Name:");
+		String lname=sc.nextLine();
+		if(lname == null)
 			System.out.println("Invalid");
-		String RegExFname = "^[A-Z]{1}[a-z]{2,}$";
-	    Pattern pattern = Pattern.compile(RegExFname);
-		if(pattern.matcher(fname).matches()) {
+		String RegExlname = "^[A-Z]{1}[a-z]{2,}$";
+	    Pattern pattern = Pattern.compile(RegExlname);
+		if(pattern.matcher(lname).matches()) {
 			System.out.println("valid");
 		} else {
 			System.out.println("Invalid"); 
