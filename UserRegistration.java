8376c49912360d88595695ce2f9fb966ab94c5ec
unknown <nigar77951@gmail.com>
2021-01-15 21:24:06 +0530

    user enters valid first name

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,5 +1,20 @@
+import java.util.regex.*;
+import java.util.*;
 public class UserRegistration {
+
 	public static void main(String[] args) {
-		System.out.println("...User Registration...");
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter first Name:");
+		String fname=sc.nextLine();
+		if(fname == null)
+			System.out.println("Invalid");
+		String RegExFname = "^[A-Z]{1}[a-z]{2,}$";
+	    Pattern pattern = Pattern.compile(RegExFname);
+		if(pattern.matcher(fname).matches()) {
+			System.out.println("valid");
+		} else {
+			System.out.println("Invalid"); 
+		}
 	}
 }
